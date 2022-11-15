5e26d62f2dcc0eff4f0c0a2d91667186aed32943
Dario <104058513+DarioIEL@users.noreply.github.com>
2022-11-15 18:36:55 +0100

    Add files via upload

--- Lezione0-Intro/js/script03_ver2.js
+++ Lezione0-Intro/js/script03_ver2.js
@@ -8,31 +8,32 @@ var nomeUtente = prompt("come ti chiami ?");
 //tutto ciò che elabora il prompt è una stringa
 //wrappando il prompt con l'interfaccia Number sto "castando" il dato, 
 //sto forzando il dato ad essere del tipo che dico io (CAST)
-var imponibile = Number( prompt("Quanto hai guadagnato questo mese ? ") );
 
-// if( typeof imponibile != "Number"){
-//     alert("Caro utente non puoi digitare delle cose che non siano numeri");
-// }
-
-const IVA = 0.22;
-
-var prezzoIvato = ((imponibile * IVA) + imponibile);
-
-var costoIVA = (prezzoIvato - imponibile);
-
-console.log("Imponibile: " + imponibile + " €");
-console.log("Iva: " + (IVA * 100) + " % " );
-console.log("Prezzo con iva: " + prezzoIvato + " €");
-console.log("Costo iva: " + costoIVA + " €");
-
-var etatraUnAnno = 33+1;
-console.log("tra un annno avrò " + 33 + 1 + " anni");
-
-
-document.write("<h2> Benvenuto utente: " + nomeUtente + "</h2>");
-document.write("<p> Imponibile: " + imponibile + " € </p>");
-document.write("<p>Iva: " + (IVA * 100) + " %</p>" );
-document.write("<p>Prezzo con iva: " + prezzoIvato + " €</p>");
-document.write("<p>Costo iva: " + costoIVA + " €</p>");
-
+if( nomeUtente.trim() == "Dario" || nomeUtente.trim() == "Anna"){
+    var imponibile = Number( prompt("Quanto hai guadagnato questo mese ? ") );
+    
+    const IVA = 0.22;
+    
+    var prezzoIvato = ((imponibile * IVA) + imponibile);
+    
+    var costoIVA = (prezzoIvato - imponibile);
+    
+    console.log("Imponibile: " + imponibile + " €");
+    console.log("Iva: " + (IVA * 100) + " % " );
+    console.log("Prezzo con iva: " + prezzoIvato + " €");
+    console.log("Costo iva: " + costoIVA + " €");
+    
+    var etatraUnAnno = 33+1;
+    console.log("tra un annno avrò " + 33 + 1 + " anni");
+    
+    
+    document.write("<h2> Benvenuto utente: " + nomeUtente + "</h2>");
+    document.write("<p> Imponibile: " + imponibile + " € </p>");
+    document.write("<p>Iva: " + (IVA * 100) + " %</p>" );
+    document.write("<p>Prezzo con iva: " + prezzoIvato + " €</p>");
+    document.write("<p>Costo iva: " + costoIVA + " €</p>");
+}else{
+    document.write("<h2>Mi spiace, non sei nei nostri database </h2>");
+}
+    
 
