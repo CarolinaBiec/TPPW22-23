ffc1ff1cdf66d0731b5a661cf32a1139a73a2806
Dario <104058513+DarioIEL@users.noreply.github.com>
2023-03-02 15:57:31 +0100

    Add files via upload

--- EsempioJSON-Server/js/carrello.js
+++ EsempioJSON-Server/js/carrello.js
@@ -1,12 +1,17 @@
 var carrello = document.querySelector("#carrello");
+var totaleCarrello = document.querySelector("#totaleCarrello");
 
 fetch("http://localhost:3000/carrello")
 .then(data =>{return data.json()})
 .then(response =>{
 
+    let totale = 0;
 
     response.forEach(prodotto => {
+        totale += prodotto.prezzo;
         carrello.innerHTML += "<li>" + prodotto.titolo + " - Prezzo: " + prodotto.prezzo + "</li>"
     });
+
+    totaleCarrello.innerHTML = "Totale: €" + totale;
 
 })
